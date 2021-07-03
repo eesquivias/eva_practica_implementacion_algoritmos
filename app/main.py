abb849d8dd1885e419cc675859a69f0f92f4ac35
eesquivias <59453939+eesquivias@users.noreply.github.com>
2021-07-03 08:53:50 +0200

    cambios clase

--- app/main.py
+++ app/main.py
@@ -6,17 +6,23 @@ from app.core.config import (
     APP_NAME,
     APP_VERSION,
     IS_DEBUG,
+    DEFAULT_MODEL_PATH,
 )
 from app.core.event_handlers import start_app_handler, stop_app_handler
 
 
 def get_app() -> FastAPI:
-    fast_app = FastAPI(title=APP_NAME, version=APP_VERSION, debug=IS_DEBUG)
-    fast_app.include_router(api_router, prefix=API_PREFIX)
-
-    fast_app.add_event_handler("startup", start_app_handler(fast_app))
-    fast_app.add_event_handler("shutdown", stop_app_handler(fast_app))
-
+    if DEFAULT_MODEL_PATH is not None:
+        fast_app = FastAPI(title=APP_NAME, version=APP_VERSION, debug=IS_DEBUG)
+        fast_app.include_router(api_router, prefix=API_PREFIX)
+        
+        fast_app.add_event_handler("startup", start_app_handler(fast_app))
+        fast_app.add_event_handler("shutdown", stop_app_handler(fast_app))
+    else:
+        raise ValueError(
+            "No model path specified. Set environment variable DEFAULT_MODEL_PATH"
+        ) 
+        
     return fast_app
 
 
